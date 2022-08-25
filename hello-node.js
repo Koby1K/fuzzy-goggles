a993d995cafecfa273d91fe73b4489aafa976ca4
Kobyingram11 <kobyingram11@gmail.com>
2022-08-25 13:43:10 -0500

    hello-node.js update

--- hello-node.js
+++ hello-node.js
@@ -4,7 +4,10 @@ const app = express()
 console.log('hello from node');
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Express')
+  // res.send('Hello World from Express')
+
+  res.sendFile(path.join(__dirname, 'index.html'));
+
 })
 
 app.listen(process.env.PORT || 3000, 
