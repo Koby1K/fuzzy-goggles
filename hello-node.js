3037be75fac71cfe1ab2badfa8477146a5c78f8c
Kobyingram11 <kobyingram11@gmail.com>
2022-08-29 20:18:45 -0500

    new json

--- hello-node.js
+++ hello-node.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require("node:path");
 
 console.log('hello from node');
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Express')
 
-  // res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.send('Hello World from Express')
 
 })
 
