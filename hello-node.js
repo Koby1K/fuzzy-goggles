61f9625b6b4beceedb82c3c22a9226abf20d0932
Kobyingram11 <kobyingram11@gmail.com>
2022-09-06 12:54:09 -0500

    updated files

--- hello-node.js
+++ hello-node.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require("node:path");
+const bodyParser = require('body-parser')
 
 console.log('hello from node');
 
 app.get('/', function (req, res) {
 
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.sendFile(path.join(__dirname, 'index.html'));
   // res.send('Hello World from Express')
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.)
 
 })
 
